cd3d6c84392e18187bef28b5fefdbe0fc36f571f
chiefkarim <krimotop2016@gmail.com>
2025-03-03 17:05:40 +0100

    feat: hilight code with rehype-hilight

--- components/markdown-wrapper.tsx
+++ components/markdown-wrapper.tsx
@@ -1,6 +1,12 @@
 import Markdown from "react-markdown";
+import rehypeHighlight from "rehype-highlight";
 import remarkGfm from "remark-gfm";
+import "highlight.js/styles/github.css";
 
 export function MarkdownWrapper({ content }: { content: string }) {
-  return <Markdown remarkPlugins={[remarkGfm]}>{content}</Markdown>;
+  return (
+    <Markdown remarkPlugins={[remarkGfm]} rehypePlugins={[rehypeHighlight]}>
+      {content}
+    </Markdown>
+  );
 }
