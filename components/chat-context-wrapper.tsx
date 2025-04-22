ec013f9d6f5003e4109b13191b37ff3aa072b943
chiefkarim <krimotop2016@gmail.com>
2025-04-22 10:11:04 +0100

    feat(retry button): remove retry button

--- components/chat-context-wrapper.tsx
+++ components/chat-context-wrapper.tsx
@@ -6,21 +6,20 @@ import { ChatContext, ChatType } from "./contexts";
 export function ChatContextWrapper({ children }: any) {
   const [chat, setChat] = useState<ChatType["chat"]>([]);
 
-  const [retry, setRetry] = useState<boolean>(false);
-  useEffect(() => {
-    const localChat = localStorage.getItem("chatHistory");
-    setChat(() => (localChat ? JSON.parse(localChat) : []));
-  }, []);
-
+  // gets chat history from localstorage
   useEffect(() => {
     const stringChat = JSON.stringify(chat);
     localStorage.setItem("chatHistory", stringChat);
   }, [chat]);
 
+  // sets chat history to localstorage
+  useEffect(() => {
+    const localChat = localStorage.getItem("chatHistory");
+    setChat(() => (localChat ? JSON.parse(localChat) : []));
+  }, []);
+
   return (
-    <ChatContext.Provider
-      value={{ chat, updateChat: setChat, retry, setRetry }}
-    >
+    <ChatContext.Provider value={{ chat, updateChat: setChat }}>
       {children}
     </ChatContext.Provider>
   );
