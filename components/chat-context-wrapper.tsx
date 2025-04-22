5250ba1df2b1fa11314f7e160a10f3b93c168c07
chiefkarim <krimotop2016@gmail.com>
2025-04-22 15:28:58 +0100

    fix: chat getting deleted after refreshing

--- components/chat-context-wrapper.tsx
+++ components/chat-context-wrapper.tsx
@@ -6,17 +6,17 @@ import { ChatContext, ChatType } from "./contexts";
 export function ChatContextWrapper({ children }: any) {
   const [chat, setChat] = useState<ChatType["chat"]>([]);
 
-  // gets chat history from localstorage
-  useEffect(() => {
-    const stringChat = JSON.stringify(chat);
-    localStorage.setItem("chatHistory", stringChat);
-  }, [chat]);
-
   // sets chat history to localstorage
   useEffect(() => {
     const localChat = localStorage.getItem("chatHistory");
     setChat(() => (localChat ? JSON.parse(localChat) : []));
   }, []);
+
+  // gets chat history from localstorage
+  useEffect(() => {
+    const stringChat = JSON.stringify(chat);
+    localStorage.setItem("chatHistory", stringChat);
+  }, [chat]);
 
   return (
     <ChatContext.Provider value={{ chat, updateChat: setChat }}>
