004214911af6e1a372bf3e44dc21fe196d56f28d
chiefkarim <krimotop2016@gmail.com>
2025-05-03 08:59:31 +0100

    feat: designed the implimentation and added inital files

---
+++ components/conversation-history.tsx
@@ -0,0 +1,13 @@
+// make an api route that you hit to get the session history
+
+// make a util that gets you the session history
+
+// this component should load the last 10 conversations
+
+// it doesn't need to be client side
+
+// shows each conversation title which is for now the first user prompt
+
+// it should recieve the conversation history as a promp to follow declaritve way
+
+// it should switch the to the relevant conversation one one of it's items are clicked
---
+++ utils/getConversations.ts
@@ -0,0 +1,21 @@
+// don't authenticate and only get the conversations for the user id passed in
+// this assures seperation of concerns by keeping authentication ceperate form  other tasks like fetching data
+
+// add start and end index and set a default start and end indexs
+
+/**
+ * Retrieves the chat history for a given user.
+ *
+ * ⚠️ **Authorization and authentication are NOT handled** in this function.
+ * Make sure the user is verified before calling it.
+ *
+ * @param  userId - The ID of the user whose chat history is being retrieved.
+ * @param  [start=0] - The starting index for the history (defaults to 0).
+ * @param  [end=10] - The end index (non-inclusive, defaults to 10).
+ * @returns  A promise that resolves to the list of chat messages.
+ */
+export async function getConversations() {
+  // get the chat history for the user with the passed id starting from index and ending at index
+}
+//TODO: another protected getConversations function could be added here that authenticates and authorizes the user before fetching the conversation history
+//TODO: limit how many messages are fetched for each conversartion
